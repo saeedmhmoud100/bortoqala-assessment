4dc2e5034fa50f81d9d378e92f27316b01e8a0cc
saeedmhmoud100 <saeedmhmoud109@gmail.com>
2024-11-17 22:27:31 +0200

    create Farm, Crop, Animal and register them in the admin panel

--- farms/admin.py
+++ farms/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Farm, Crop, Animal
+
+
+class FarmAdmin(admin.ModelAdmin):
+    list_display = ('name', 'location', 'size', 'owner')
+    list_filter = ('location', 'owner')
+
+
+class CropAdmin(admin.ModelAdmin):
+    list_display = ('name', 'type', 'planting_date', 'harvest_date', 'farm')
+    list_filter = ('type', 'planting_date', 'harvest_date', 'farm')
+
+
+class AnimalAdmin(admin.ModelAdmin):
+    list_display = ('name', 'species', 'birth_date', 'farm')
+    list_filter = ('species', 'birth_date', 'farm')
+
+
+admin.site.register(Farm, FarmAdmin)
+admin.site.register(Crop, CropAdmin)
+admin.site.register(Animal, AnimalAdmin)
--- farms/models.py
+++ farms/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
 
 # Create your models here.
+
+
+class Farm(models.Model):
+    owner = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+    size = models.FloatField()
+
+    def __str__(self):
+        return self.name
+
+
+class Crop(models.Model):
+    farm = models.ForeignKey(Farm, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    type = models.CharField(max_length=255)
+    planting_date = models.DateField()
+    harvest_date = models.DateField()
+
+    def __str__(self):
+        return self.name
+
+
+class Animal(models.Model):
+    farm = models.ForeignKey(Farm, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    species = models.CharField(max_length=255)
+    birth_date = models.DateField()
+
+    def __str__(self):
+        return self.name
