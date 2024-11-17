b618c51e05ddb1735fbe9c368950b1bcf6da0a14
saeedmhmoud100 <saeedmhmoud109@gmail.com>
2024-11-17 20:28:29 +0200

    create custom user model, register it in admin panel

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+User = get_user_model()
+
+
+class CustomUserAdmin(UserAdmin):
+    model = User
+    fieldsets = (
+        *UserAdmin.fieldsets,
+        (
+            "personal info",
+            {
+                'fields': (
+                    'phone_number',
+                    'address',
+                )
+            }
+        )
+    )
+
+
+admin.site.register(User, CustomUserAdmin)
